b7b3d6e98d866e439407dbebd0416cc90ba602e6
5akurasi <5akurasilegends@gmail.com>
2025-02-24 16:19:05 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+document.getElementById("log1").addEventListener("click", function () {
+    window.location.href = "login.html";
+});
---
+++ JS/scriptlog.js
@@ -0,0 +1,3 @@
+document.getElementById("reg1").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
